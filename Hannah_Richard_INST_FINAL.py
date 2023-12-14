64278d75f9b116a214cd513df604fd9823aa78a5
Hannah Richard <hannah13richard@gmail.com>
2023-12-14 14:54:09 -0500

    edited code to handle missing data

--- Hannah_Richard_INST_FINAL.py
+++ Hannah_Richard_INST_FINAL.py
@@ -7,15 +7,8 @@ makeup_page = requests.get("http://makeup-api.herokuapp.com/api/v1/products.json
 makeup_data = json.loads(makeup_page.content)
 
 makeup_dict = {}
-user_brand = input("Which makeup brand would you like to search for?\nYour options include: 'almay', 'alva', 'clinique', 'benefit', 'covergirl', 'e.l.f.', 'essie', 'fenty', 'gossier', iman', 'marcelle', 'maybelline', 'misa', 'mistura', 'revlon', smashbox'\n")
 user_product = input("Which make product would you like to search for?\nYour options include: 'Blush', 'Bronzer', 'Eyeline', 'Eyebrow', 'Eyeshadow', 'Foundation', 'Lip Liner', 'Lipstick', 'Mascara', 'Nail Polish'\n").lower()
 user_price = float(input("What is the maximum price you are willing to pay for a makeup product?\nEnter a number:\n"))
-
-print(makeup_data[0].keys())
-for idx in range(len(makeup_data)):
-    print(makeup_data[idx]["name"])
-    makeup_data[idx]["product_type"]
-
 
 for idx in range(len(makeup_data)):
     product_id = makeup_data[idx]["product_type"]
@@ -23,10 +16,14 @@ for idx in range(len(makeup_data)):
         product = str(makeup_data[idx]["name"])
         price = str(makeup_data[idx]["price"])
         makeup_dict[product] = price
-makeup_dict.keys()
-makeup_dict.values()
+
+
+print(makeup_dict)
 for key, values in makeup_dict.items():
-    product_price = float(values)
+    if values == "None":
+        product_price = 0.0
+    else:
+        product_price = float(values)
     if product_price < user_price:
         print("The item {}is in your price range and it costs: {}".format(key, values))
 
