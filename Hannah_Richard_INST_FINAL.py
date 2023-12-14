41828824b2bc172325dc7af2c098ae8d63264659
Hannah Richard <hannah13richard@gmail.com>
2023-12-14 14:35:29 -0500

    Added a dictionary to organize data

--- Hannah_Richard_INST_FINAL.py
+++ Hannah_Richard_INST_FINAL.py
@@ -6,20 +6,24 @@ import json # base.package to work with JSON data https://docs.python.org/3/libr
 makeup_page = requests.get("http://makeup-api.herokuapp.com/api/v1/products.json")
 makeup_data = json.loads(makeup_page.content)
 
-
+makeup_dict = {}
 user_brand = input("Which makeup brand woudl you like to search for?\nYour options include: 'almay', 'alva', 'clinique', 'benefit', 'covergirl', 'e.l.f.', 'essie', 'fenty', 'gossier', iman', 'marcelle', 'maybelline', 'misa', 'mistura', 'revlon', smashbox'\n")
 user_product = input("Which make product would you like to search for?\nYour options include: 'Blush', 'Bronzer', 'Eyeline', 'Eyebrow', 'Eyeshadow', 'Foundation', 'Lip Liner', 'Lipstick', 'Mascara', 'Nail Polish'\n")
 user_price = float(input("What is the maximum price you are willing to pay for a makeup product?\nEnter a number:\n"))
 
 
-type(makeup_data)
-len(makeup_data)
-print(makeup_data[0])
-
-
 for idx in range(len(makeup_data)):
     brand_id = makeup_data[idx]["brand"]
-    print(brand_id)
+    if brand_id == user_brand:
+        product = str(makeup_data[idx]["name"])
+        price = str(makeup_data[idx]["price"])
+        makeup_dict[product] = price
+makeup_dict.keys()
+makeup_dict.values()
+for key, values in makeup_dict.items():
+    product_price = float(values)
+    if product_price < user_price:
+        print("The item {}is in your price range and it costs: {}".format(key, values))
 
-    
+
 
